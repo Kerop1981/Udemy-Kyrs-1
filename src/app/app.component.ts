a90e98a8f4bf1d7b86596b79170912f9175e22af
Kerop1981 <vova.abraamyan@mail.ru>
2024-04-29 18:14:35 +0300

    тема service:90 -100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './new-account/accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ 
 })
-export class AppComponent {
-  title = 'Udemy-Kyrs-1';
+export class AppComponent implements OnInit{
+ accounts : {name:string,status:string}[] = [];
+
+ constructor(private accountsService:AccountsService){}
+  
+ ngOnInit() {
+   this.accounts = this.accountsService.accounts;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountComponent } from './account/account.component';
+import { AccountsService } from './new-account/accounts.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    NewAccountComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AccountsService,LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
